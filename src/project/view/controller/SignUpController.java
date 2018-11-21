8652c339110bdb606fdc0913942544a67d811ec4
Gorka <redon.d.gorka@gmail.com>
2018-11-21 14:05:05 +0100

    Desktop application post correcion javi

--- src/project/view/controller/SignUpController.java
+++ src/project/view/controller/SignUpController.java
@@ -38,57 +38,108 @@ import message.User;
  * @author Gorka
  */
 public class SignUpController {
-    
+    /**
+     * Logger
+     */
     private static final Logger LOG = Logger.getLogger("view.controller.SignUpController");
-    
+    /**
+     * Campo nombre completo
+     */
     @FXML
     private TextField txtFName;
+    /**
+     * Campo email
+     */
     @FXML
     private TextField txtFEmail;
+    /**
+     * Campo usuario
+     */
     @FXML
     private TextField txtFUser;
+    /**
+     * Campo contraseña visible
+     */
     @FXML
     private TextField txtFPassword;
+    /**
+     * Campo repite contraseña visible
+     */
     @FXML
     private TextField txtFRpPassword;
+    /**
+     * Campo contraseña
+     */
     @FXML
     private PasswordField pwPassword;
+    /**
+     * Campo repite contraseña
+     */
     @FXML
     private PasswordField pwRpPassword;
+    /**
+     * Boton registrase
+     */
     @FXML
     private Button btnSignUp;
+    /**
+     * Boton cancelar
+     */
     @FXML
     private Button btnCancel;
+    /**
+     * Boton ver contraseña
+     */
     @FXML
     private Button btnPassShow;
+    /**
+     * Boton ver contraseña repetida
+     */
     @FXML
     private Button btnRpPassShow;
+    /**
+     * Label email
+     */
     @FXML
     private Label lblEmail;
+    /**
+     * Label usuario
+     */
     @FXML
     private Label lblUser;
+    /**
+     * Label contraseña
+     */
     @FXML
     private Label lblPassword;
+    /**
+     * Label repetir contraseña
+     */
     @FXML
     private Label lblRpPassword;
     
-    private String errorMessage;
-    
+    /**
+     * Atributo logica
+     */
     private Logic logic;
-    
-    private Stage stage;
-    public Stage getStage(){
-        return stage;
+    public void setLogic(Logic logic) {
+        this.logic=logic;
     }
+    
+    /**
+     * Atributo ventana
+     */
+    private Stage stage;
     public void setStage(Stage stage){
         this.stage=stage;
     }
     
     /**
      * Inilization of the window
-     * @param root 
+     * @param root Contenedor
      */
     public void initStage(Parent root){
+        LOG.info("Initializing Signup stage");
         //Create a scene associated to the node grap root
         Scene scene = new Scene(root);
         //Associate scene to Stage(Window)
@@ -100,11 +151,11 @@ public class SignUpController {
         stage.setOnShowing(this::handleWindowShowing);
         //Set control events
         btnSignUp.setOnAction(this::pushSignUp);
+        btnCancel.setOnAction(this::pushCancel);
         btnPassShow.setOnMousePressed(this::pressPassShow);
         btnPassShow.setOnMouseReleased(this::relasePassShow);
         btnRpPassShow.setOnMousePressed(this::pressPassShow);
         btnRpPassShow.setOnMouseReleased(this::relasePassShow);
-        btnCancel.setOnAction(this::pushCancel);
         txtFName.textProperty().addListener(this::textChanged);
         txtFEmail.textProperty().addListener(this::textChanged);
         txtFUser.textProperty().addListener(this::textChanged);
@@ -115,8 +166,8 @@ public class SignUpController {
     }
     
     /**
-     * 
-     * @param event 
+     * Creates a mnemonic to SignUp and Cancel button
+     * @param event Un evento
      */
     public void handleWindowShowing(WindowEvent event){
         LOG.info("Beginning handleWindowShowing");
@@ -131,53 +182,62 @@ public class SignUpController {
     /**
      * Text changed of the fields Full name, Email, Username, Password y
      * Repeat Password
-     * @param observable
-     * @param oldValue
-     * @param newValue 
+     * @param observable Parametro que comprueba si el valor viejo cambia
+     * @param oldValue Valor viejo
+     * @param newValue Valor nuevo
      */
      private void textChanged(ObservableValue observable, String oldValue,
             String newValue) {
-         if(!txtFName.getText().trim().isEmpty() && !txtFEmail.getText().trim().isEmpty()
-                && !txtFUser.getText().trim().isEmpty() && !pwPassword.getText().trim().isEmpty()
-                && !pwRpPassword.getText().trim().isEmpty()){
-            btnSignUp.setDisable(false);
-         }else {
-            btnSignUp.setDisable(true);
-         }
-         
-         if(txtFName.getLength() == 101){
+        //If all textfields are not empty, set enable the SignUp button
+        if(!txtFName.getText().trim().isEmpty() && !txtFEmail.getText().trim().isEmpty()
+               && !txtFUser.getText().trim().isEmpty() && !pwPassword.getText().trim().isEmpty()
+               && !pwRpPassword.getText().trim().isEmpty()){
+           btnSignUp.setDisable(false);
+        }else {
+           btnSignUp.setDisable(true);
+        }
+        
+        //Test if the name is too long
+        if(txtFName.getLength() == 101){
+            LOG.info("Name too long");
             txtFName.setText(txtFName.getText().substring(0,100));
             new Alert(AlertType.INFORMATION,"Name too long",ButtonType.OK).show();
-         }
-         
-         
-         if(txtFEmail.getLength() == 51){
+        }
+        
+        //Test if the email is too long
+        if(txtFEmail.getLength() == 51){
+            LOG.info("Email too long");
             txtFEmail.setText(txtFEmail.getText().substring(0,50));
             new Alert(AlertType.INFORMATION,"Email too long",ButtonType.OK).show();
-         }
-         
-         if(txtFUser.getLength() == 21){
+        }
+        
+        //Test if the username is too long
+        if(txtFUser.getLength() == 21){
+            LOG.info("User too long");
             txtFUser.setText(txtFUser.getText().substring(0,20));
             new Alert(AlertType.INFORMATION,"User too long",ButtonType.OK).show();
-         }
-         
-         if(pwPassword.getLength() == 17){
+        }
+        
+        //Test if the password is too long
+        if(pwPassword.getLength() == 17){
+            LOG.info("Password too long");
             pwPassword.setText(pwPassword.getText().substring(0,16));
             new Alert(AlertType.INFORMATION,"Password too long",ButtonType.OK).show();
-         }
-         
-         if(pwRpPassword.getLength() == 17){
+        }
+        
+        //Test if the repeat password is too long
+        if(pwRpPassword.getLength() == 17){
+            LOG.info("Password too long");
             pwRpPassword.setText(pwRpPassword.getText().substring(0,16));
             new Alert(AlertType.INFORMATION,"Password too long",ButtonType.OK).show();
-         } 
+        } 
      }
     
     /**
      * Pulse SignUp button
-     * @param event 
+     * @param event Un evento
      */
     private void pushSignUp(ActionEvent event){
-        errorMessage = "";
         if(verifyEmail() && verifyUser() && verifyPassword()){
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
             User user = new User(Status.enabled, Privilege.user);
@@ -189,27 +249,29 @@ public class SignUpController {
             user.setLastPasswordChange(timestamp);
             try {
                 logic.signUpUser(user);
-                new Alert(AlertType.CONFIRMATION,"Register confirmed",ButtonType.OK).showAndWait();
+                LOG.info("Your registration has been completed successfully");
+                new Alert(AlertType.CONFIRMATION,"Your registration has been "
+                        + "completed successfully",ButtonType.OK).showAndWait();
                 openLogIn();
             } catch (LoginExistingException lee) {
-                LOG.log(Level.SEVERE, lee.getMESSAGE(), lee);
+                LOG.log(Level.SEVERE, lee.getMESSAGE());
                 lblUser.setTextFill(Color.web("#FF0000"));
                 new Alert(AlertType.ERROR,lee.getMESSAGE(), ButtonType.OK).showAndWait();
+                txtFUser.requestFocus();
             } catch (EmailNotUniqueException enue) {
-                LOG.log(Level.SEVERE, enue.getMESSAGE(), enue);
+                LOG.log(Level.SEVERE, enue.getMESSAGE());
                 lblEmail.setTextFill(Color.web("#FF0000"));
                 new Alert(AlertType.ERROR,enue.getMESSAGE(), ButtonType.OK).showAndWait();
+                txtFEmail.requestFocus();
             } catch (Exception ex) {
-                LOG.log(Level.SEVERE, ex.getMessage(), ex);
+                LOG.log(Level.SEVERE, ex.getMessage());
             }
-        }else {
-            new Alert(AlertType.ERROR,errorMessage,ButtonType.OK).showAndWait();
         }
     }
     
     /**
      * Mouse down PassShow button
-     * @param event 
+     * @param event Un evento
      */
     private void pressPassShow(MouseEvent event){
         if(btnPassShow.isPressed()){
@@ -225,7 +287,7 @@ public class SignUpController {
     
     /**
      * Mouse up PassShow button
-     * @param event 
+     * @param event Un evento
      */
     private void relasePassShow(MouseEvent event){
         pwPassword.setVisible(true);
@@ -237,14 +299,14 @@ public class SignUpController {
     
     /**
      * Pulse Cancel button
-     * @param event 
+     * @param event An event
      */
     private void pushCancel(ActionEvent event){
         openLogIn();
     }
+    
     /**
      * Method to open LogIn window and close SignUp
-     * @return boolean
      */
     private void openLogIn() {
         try{
@@ -257,15 +319,16 @@ public class SignUpController {
             controller.setLogic(logic);
             controller.initStage(root);
             stage.hide();
+            LOG.info("LogIn window opened successfully");
         }catch(IOException e){
-            LOG.log(Level.SEVERE, e.getMessage(), e);
+            LOG.log(Level.SEVERE, e.getMessage());
             new Alert(AlertType.ERROR,e.getMessage(),ButtonType.OK).show();
         }
     }
     
     /**
      * Method to validate email
-     * @return boolean
+     * @return boolean Es un tipo de dato de true or false
      */
     private boolean verifyEmail() {
         boolean a = false;
@@ -282,73 +345,57 @@ public class SignUpController {
             lblEmail.setTextFill(Color.web("#237bf7"));
             return true;
         }else {
+            LOG.info("Your email have wrong format. Correct format is: "
+                    + "example@example.example");
             lblEmail.setTextFill(Color.web("#FF0000"));
-            errorMessage = "Your email have wrong format. Correct format is: "
-                    + "example@example.example";
+            new Alert(AlertType.ERROR,"Your email have wrong format. Correct "
+                    + "format is: example@example.example",ButtonType.OK).showAndWait();
+            txtFEmail.requestFocus();
             return false;
         }
     }
     
     /**
      * Method to validate user
-     * @return boolean
+     * @return boolean Es un tipo de dato de true or false
      */
     private boolean verifyUser() {
         if(txtFUser.getLength() >= 4){
             lblUser.setTextFill(Color.web("#237bf7"));
             return true;
         }else {
+            LOG.info("Username too short");
             lblUser.setTextFill(Color.web("#FF0000"));
-            if(errorMessage.isEmpty()){
-                errorMessage = "Username too short";
-            }else {
-                errorMessage = errorMessage + "\nUsername too short";
-            }
+            new Alert(AlertType.ERROR,"Username too short",ButtonType.OK).showAndWait();
+            txtFUser.requestFocus();
             return false;
         }
     }
     
     /**
      * Method to validate password
-     * @return boolean
+     * @return boolean Es un tipo de dato de true or false
      */
     private boolean verifyPassword() {
-        if(pwPassword.getLength() >=4){
+        if(pwPassword.getLength() >= 4){
+            lblPassword.setTextFill(Color.web("#237bf7"));
             if(pwPassword.getText().equals(pwRpPassword.getText())){
-                lblPassword.setTextFill(Color.web("#237bf7"));
                 lblRpPassword.setTextFill(Color.web("#237bf7"));
                 return true;
             }else {
-                lblPassword.setTextFill(Color.web("#FF0000"));
+                LOG.info("Password do not match");
                 lblRpPassword.setTextFill(Color.web("#FF0000"));
-                if(errorMessage.isEmpty()){
-                    errorMessage = "Password no coinciden";
-                }else {
-                    errorMessage = errorMessage + "\nPassword no coinciden";
-                }
+                new Alert(AlertType.ERROR,"Password do not match",ButtonType.OK).showAndWait();
+                pwRpPassword.requestFocus();
                 return false;
             }
         }else {
-            pwPassword.clear();
-            pwRpPassword.clear();
+            LOG.info("Password too short");
             lblPassword.setTextFill(Color.web("#FF0000"));
             lblRpPassword.setTextFill(Color.web("#FF0000"));
-            if(errorMessage.isEmpty()){
-                errorMessage = "Password too shot";
-            }else {
-                errorMessage = errorMessage + "\nPassword too short";
-            }
+            new Alert(AlertType.ERROR,"Password too short",ButtonType.OK).showAndWait();
+            pwPassword.requestFocus();
             return false;
         }
-        
-        
-    }
-    
-    /**
-     * 
-     * @param logic 
-     */
-    public void setLogic(Logic logic) {
-        this.logic=logic;
     }
 }
